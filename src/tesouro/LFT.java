48773eb8b90f7d839f8d20e6b5a38c4968ca7d9f
Pedro Sales <pedrohsalesf@gmail.com>
2017-05-16 23:26:00 -0300

    LTF - pass test calculaTaxaSelicDoDiaArredondamento

--- src/tesouro/LFT.java
+++ src/tesouro/LFT.java
@@ -1,19 +1,24 @@
 package tesouro;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 import java.math.RoundingMode;
 
 public class LFT {
 
     private static int TAXA_DIVULGADA_PRECISION = 2;
+    private static int TAXA_SELIC_DO_DIA_PRECISION = 8;
     double taxaDivulgada;
 
 
     public double calculaTaxaSelicDoDia(double taxaDivulgada) {
         setTaxaDivulgada(taxaDivulgada);
-        double taxaSelic = Math.pow(((taxaDivulgada/100) + 1), (1/252)) - 1;
-        return taxaSelic;
+        double taxaDivulgadaPerCentPlus = (getTaxaDivulgada()/100) + 1;
+        double expoente = new BigDecimal(1).divide(new BigDecimal(252), new MathContext(100)).doubleValue();
+        return Math.pow(taxaDivulgadaPerCentPlus, expoente) - 1;
     }
+
+
 
     public void setTaxaDivulgada(double taxaDivulgada) {
         BigDecimal bigDecimalValue = new BigDecimal(taxaDivulgada);
