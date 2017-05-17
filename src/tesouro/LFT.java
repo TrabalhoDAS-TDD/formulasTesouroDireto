25f30e7c2f0abbe158a1112b9d98184c12f32daf
Pedro Sales <pedrohsalesf@gmail.com>
2017-05-16 22:04:01 -0300

    LTF - implement calculaTaxaSelicDoDia not null

--- src/tesouro/LFT.java
+++ src/tesouro/LFT.java
@@ -1,6 +1,11 @@
 package tesouro;
 
 
+import java.math.BigDecimal;
+
 public class LFT {
 
+    public BigDecimal calculaTaxaSelicDoDia(BigDecimal taxaDivulgada) {
+        return new BigDecimal(11.25);
+    }
 }
