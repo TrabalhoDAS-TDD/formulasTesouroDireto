79f35cdd58874b25920a74ee38ce6f475305e70f
Pedro Sales <pedrohsalesf@gmail.com>
2017-05-16 21:34:15 -0300

    Create LFT - calculaTaxaSelicDoDia test

---
+++ tests/tesouro/LFTTest.java
@@ -0,0 +1,25 @@
+package tesouro;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class LFTTest {
+
+    LFT lft;
+
+    @Before
+    public void setUp(){
+        lft = new LFT();
+    }
+
+    @Test
+    public void calculaTaxaSelicDoDiaTest(){
+        fail("Not implemented");
+    }
+
+
+
+}
