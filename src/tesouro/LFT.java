e30366f61ccc49c126bcff80cb602a1d25668609
Pedro Sales <pedrohsalesf@gmail.com>
2017-05-16 22:47:10 -0300

    LTF - implement rouding of taxaDivulgada

--- src/tesouro/LFT.java
+++ src/tesouro/LFT.java
@@ -1,10 +1,26 @@
 package tesouro;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class LFT {
 
-    public double calculaTaxaSelicDoDia(double taxaDivulgada) {
+    private static int TAXA_DIVULGADA_PRECISION = 2;
+    double taxaDivulgada;
 
+
+    public double calculaTaxaSelicDoDia(double taxaDivulgada) {
+        setTaxaDivulgada(taxaDivulgada);
         double taxaSelic = Math.pow(((taxaDivulgada/100) + 1), (1/252)) - 1;
         return taxaSelic;
+    }
+
+    public void setTaxaDivulgada(double taxaDivulgada) {
+        BigDecimal bigDecimalValue = new BigDecimal(taxaDivulgada);
+        this.taxaDivulgada = bigDecimalValue.setScale(TAXA_DIVULGADA_PRECISION, RoundingMode.FLOOR).doubleValue();
+    }
+
+    public double getTaxaDivulgada() {
+        return taxaDivulgada;
     }
 }
