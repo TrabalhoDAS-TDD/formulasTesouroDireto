791d4fd639d9fae4aa2922ea7fbee01c5e97a6ad
Lucas Couto <loc.unb@gmail.com>
2017-05-17 16:42:17 -0300

    - Implementing test for "CalculaJuros" - Broken test

--- src/tesouro/BTN.java
+++ src/tesouro/BTN.java
@@ -8,13 +8,9 @@ public class BTN {
 	private static int PRECO_UNITARIO_PRECISION = 6;
 	
 	public double calculaPrecoUnitario(double PUAnterior, double TR){	
-		
-		
-		
+	
 		double PUAtualizado = new BigDecimal(PUAnterior).setScale(PRECO_UNITARIO_PRECISION, RoundingMode.FLOOR).doubleValue() * TR;
 		return new BigDecimal(PUAtualizado).setScale(PRECO_UNITARIO_PRECISION, RoundingMode.FLOOR).doubleValue();
 		
 	}
-
-
 }
--- tests/tesouro/BTNTest.java
+++ tests/tesouro/BTNTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 public class BTNTest {
 	
 	BTN btn;
-	private static final double PRECISION_PU = 0.01;
+	private static final double PRECISION = 0.01;
 
 	@Before
 	public void setUp() throws Exception {
@@ -16,9 +16,16 @@ public class BTNTest {
 	}
 
 	@Test
-	public void test() {
+	public void calculaPrecoUnitarioTest() {
 		double precoUnitario = btn.calculaPrecoUnitario(2456.23423423, 0.0764);
-		assertEquals(187.656295, precoUnitario, PRECISION_PU);
+		assertEquals(187.656295, precoUnitario, PRECISION);
+	}
+	
+	@Test
+	public void calculaJurosTest(){
+		double juros = btn.calculaJuros();
+		assertEquals(0, juros, PRECISION);
+		
 	}
 
 }
