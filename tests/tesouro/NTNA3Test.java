6af4a4bfd3c03c7f2f2044bb7c06a24f6c18ea4b
Lucas Couto <loc.unb@gmail.com>
2017-05-22 18:36:24 -0300

    Creating a first version of test for calculaJuros() method
     - Method not implemented
     - Test failing

--- tests/tesouro/NTNA3Test.java
+++ tests/tesouro/NTNA3Test.java
@@ -20,5 +20,11 @@ public class NTNA3Test {
 		double valorNominal = ntn.calculaValorNominal(2.849, 2.512);
 		assertEquals(1134.156050, valorNominal, PRECISION6);
 	}
+	
+	@Test
+	public void calculaJurosTest(){
+		double juros = ntn.calculaJuros();
+		assertEquals(0, juros, PRECISION6);
+	}
 
 }
