d7ca729d3fb8b6495a3d5b3615845219f7cfc0f2
Lucas Couto <loc.unb@gmail.com>
2017-05-22 18:00:10 -0300

    Implementing calculaValorNominal() method
     - Test passing

--- src/tesouro/NTNA3.java
+++ src/tesouro/NTNA3.java
@@ -1,10 +1,14 @@
 package tesouro;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class NTNA3 {
 
-	public double calculaValorNominal(double dolarA, double dolarB) {
+	public double calculaValorNominal(double dolarA, double dolarB) {	
+		double valorNominal = dolarA/dolarB*1000;
+		return new BigDecimal(valorNominal).setScale(6, RoundingMode.FLOOR).doubleValue();
 		
-		return 0;
 	}
 
 }
