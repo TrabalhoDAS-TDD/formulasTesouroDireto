11b0a9f103d4c3cdbbda1e4bea859e32cc524c22
Lucas Couto <loc.unb@gmail.com>
2017-05-22 10:27:16 -0300

    Adding comparision to precoUnitario with a diferent precision in calculaPrecoUnitarioAmortizacaoTest()
     - Test passing

--- tests/tesouro/NTNA1Test.java
+++ tests/tesouro/NTNA1Test.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 public class NTNA1Test {
 	
 	NTNA1 ntn;
+	private static final double PRECISION8 = 0.00000001;
 	private static final double PRECISION6 = 0.000001;
 
 	@Before
@@ -19,6 +20,7 @@ public class NTNA1Test {
 	public void calculaPrecoUnitarioAmortizacaoTest() {
 		double precoUnitario = ntn.calculaPrecoUnitarioAmortizacao(2.849, 2.512, 2345.58, 0.35);
 		assertEquals(931.0888125, precoUnitario, PRECISION6);
+		assertNotEquals(931.0888125, precoUnitario, PRECISION8);
 	}
 
 }
