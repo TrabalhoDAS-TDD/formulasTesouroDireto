aee1598fd3964d3e8402c1c494c76d204e968b46
Lucas Couto <loc.unb@gmail.com>
2017-05-22 10:22:43 -0300

    Adding rounding mode in calculaPrecoUnitarioAmortizacao() method
     - Test Passing

--- src/tesouro/NTNA1.java
+++ src/tesouro/NTNA1.java
@@ -1,15 +1,16 @@
 package tesouro;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class NTNA1 {
 
 	public double calculaPrecoUnitarioAmortizacao(double dolarA, double dolarB, double valorEmissao, double percentAmortizacao) {
-		
-	
-		return dolarA/dolarB * this.calculaVL(valorEmissao, percentAmortizacao);
+		double precoUnitario = dolarA/dolarB * this.calculaVL(valorEmissao, percentAmortizacao);
+		return new BigDecimal(precoUnitario).setScale(6, RoundingMode.FLOOR).doubleValue();
 	}
 	
 	private double calculaVL(double valorEmissao, double percentAmortizacao){
-		
 		return valorEmissao*percentAmortizacao;
 	}
 }
