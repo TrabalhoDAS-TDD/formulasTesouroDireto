1c32504270452f608a67c6a1519916386b60dd9b
Lucas Couto <loc.unb@gmail.com>
2017-05-22 09:23:53 -0300

    Preparing test for calculaPrecoUnitarioAmortizacao() method
     - Class NTNA1 implemented
     - Method not implemented
     - Test failing

---
+++ src/tesouro/NTNA1.java
@@ -0,0 +1,5 @@
+package tesouro;
+
+public class NTNA1 {
+
+}
---
+++ tests/tesouro/NTNA1Test.java
@@ -0,0 +1,22 @@
+package tesouro;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class NTNA1Test {
+	
+	NTNA1 ntn;
+
+	@Before
+	public void setUp() throws Exception {
+		ntn = new NTNA1();
+	}
+
+	@Test
+	public void calculaPrecoUnitarioAmortizacaoTest() {
+		fail("Not yet implemented");
+	}
+
+}
