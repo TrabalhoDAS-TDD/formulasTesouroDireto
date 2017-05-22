5fe914c0866fbc1dc9b7e6b0ca29711d9e7df069
Pedro Sales <pedrohsalesf@gmail.com>
2017-05-22 20:45:56 -0300

    NTNB - improve calculaFator method

--- src/tesouro/NTNB.java
+++ src/tesouro/NTNB.java
@@ -1,8 +1,15 @@
 package tesouro;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+
 public class NTNB {
 
-    public double calculaFator() {
-        return 0;
+    public double calculaFator(double ipca, double ipcai, double dc, double dct) {
+
+        double expoente = new BigDecimal(dc).divide( new BigDecimal(dct), new MathContext(100)).doubleValue();
+        double base = new BigDecimal(ipca).divide(new BigDecimal(ipcai), new MathContext(100)).doubleValue();
+        return Math.pow(base, expoente);
+
     }
 }
