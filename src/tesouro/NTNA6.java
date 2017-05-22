0c351af383f74da0ec8ef8de4726c75f1325003c
Lucas Couto <loc.unb@gmail.com>
2017-05-22 20:12:05 -0300

    Implementing calculaValorNominalAtualizado() method
     - test passing

--- src/tesouro/NTNA6.java
+++ src/tesouro/NTNA6.java
@@ -1,9 +1,13 @@
 package tesouro;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class NTNA6 {
 
 	public double calculaValorNominalAtualizado(double dolarA, double dolarB, double valorEmissao) {
-		return 0;
+		BigDecimal valorNominal = new BigDecimal(dolarA/dolarB*valorEmissao);
+		return valorNominal.setScale(6, RoundingMode.FLOOR).doubleValue();
 	}
 
 }
