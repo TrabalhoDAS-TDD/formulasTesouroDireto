a5bf3c62c17c71f4a87e5df273fd130c5d0af6c8
Lucas Couto <loc.unb@gmail.com>
2017-05-22 21:25:26 -0300

    Implementing comparasion to other precision and rouding precoUnitario value

--- src/tesouro/LTN.java
+++ src/tesouro/LTN.java
@@ -1,5 +1,8 @@
 package tesouro;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class LTN {
 
 	public double calculaPrecoUnitario(double taxaIndicativa) {
@@ -7,7 +10,7 @@ public class LTN {
 		double denominador = Math.pow(1 + taxaIndicativa, (double)1/252);
 		double precoUnitario = 1000/denominador;
 		
-		return precoUnitario;
+		return new BigDecimal(precoUnitario).setScale(6, RoundingMode.FLOOR).doubleValue();
 	}
 
 }
--- tests/tesouro/LTNTest.java
+++ tests/tesouro/LTNTest.java
@@ -9,6 +9,7 @@ public class LTNTest {
 	
 	LTN ltn;
 	private static double PRECISION6 = 0.000001;
+	private static double PRECISION8 = 0.00000001;
 
 	@Before
 	public void setUp() throws Exception{
@@ -20,6 +21,7 @@ public class LTNTest {
 	public void calculaPrecoUnitarioTest() {
 		double precoUnitario = ltn.calculaPrecoUnitario(9.7351);
 		assertEquals(990.6254920747908, precoUnitario, PRECISION6);
+		assertNotEquals(990.6254920747908, precoUnitario, PRECISION8);
 	}
 
 }
